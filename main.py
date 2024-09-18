bce2078107e8cadedd601aa8da433dc7e50fff63
KimARam <111050789+KIM-ARAM@users.noreply.github.com>
2024-09-18 09:48:13 +0900

    Update main.py
    크게 건든 부분 없이 통신 파트 수정하고 db 재연결될 수 있도록만 수정했어요

--- main.py
+++ main.py
@@ -1,47 +1,30 @@
 import os
 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
-from langchain_core.pydantic_v1 import BaseModel, Field
 
 from openAI_api import llm
-from access_milvusDB import db
+from access_milvusDB import MilvusDB
 from available_functions import callable_tools
 
-# langsmith, langchain 환경 변수 설정
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = "본인의 lanchain api key 입력"
-os.environ["LANGCHAIN_PROJECT"] = "langchain 프로젝트 이름 입력"
+os.environ["LANGCHAIN_API_KEY"] = "api_key"
+os.environ["LANGCHAIN_PROJECT"] = "프로젝트이름"
 
+app = FastAPI()
 
-def ask_ai(agent, config):
-    while True:
-        # 사용자 질문 받기 부분 - q라고 쓰면 대화 종료
-        question = input("사용자 : ")
-
-        if(question == 'q'):
-            # DB 연결 끊기
-            db.unconnect()
-            break
-
-        # 에이전트 실행
-        response = agent.invoke({"messages": [("human", f"{question}")]}, config)
-        ai_answer = response["messages"][-1].content
-        print("ai: ", ai_answer)
-        
-        
-
-# 대화의 초기 프롬프트 생성
 initial_system_prompt = "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
 
-# 대화 기록 메모리 생성
 memory = MemorySaver()
 
-# 함수 호출 도구 준비
 tools = callable_tools
 
-# 함수 호출 에이전트 생성 - 호출 가능한 함수에 대한 정보를 전달
+# 에이전트 생성
 agent = create_react_agent(
     llm, 
     tools, 
@@ -51,6 +34,34 @@ agent = create_react_agent(
 
 config = {"configurable": {"thread_id": "test-thread"}}
 
-# 대화 메인 실행
-ask_ai(agent, config)
+class QuestionRequest(BaseModel):
+    question: str
 
+
+class AiResponse(BaseModel):
+    answer: str
+    place: Optional[List[Dict]] = None
+    
+db = MilvusDB()
+
+
+@app.post("/ask-ai/", response_model=AiResponse)
+async def ask_ai(request: QuestionRequest):
+    question = request.question  # JSON에서 question 필드 추출
+
+    try:
+        db.reconnect()
+        # 에이전트 실행
+        response = agent.invoke({"messages": [("human", f"{question}")]}, config)
+        ai_answer = response["messages"][-1].content
+        print(AiResponse(answer=ai_answer))
+        return AiResponse(answer=ai_answer)
+    except Exception as e:
+        print("에러 발생: ", e)
+        raise HTTPException(status_code=500, detail="AI 처리 중 오류 발생")
+    finally:
+        db.unconnect()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
